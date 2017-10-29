bc8187777e57c6e9db70ec9cec934ba141f80af7
mcortex <cortexxx.bomb@gmail.com>
2017-10-30 02:53:36 +0300

    Se agrega control de SIGINT

--- Servo/PulsadorServo.py
+++ Servo/PulsadorServo.py
@@ -1,7 +1,8 @@
 import RPi.GPIO as GPIO
 import time
 import serial
-
+import signal
+import sys
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(24, GPIO.IN)
@@ -10,6 +11,15 @@ arduino = serial.Serial('/dev/ttyACM0', 9600) #Objeto arduino linkeado al puerto
 count = 0
 estado = arduino.readline()
 
+
+### FUNCIONES:
+def signal_handler(signal, frame):
+    print('Ha Presionado Ctrl+C, saliendo')
+    arduino.close()
+    GPIO.cleanup()
+    sys.exit(0)
+
+print("Inicializando escucha. Con Ctrl+C sale del programa")
 print("Estado de la puerta: " + str(estado))
 
 while True:
@@ -18,9 +28,9 @@ while True:
         count = count + 1
         print("Boton presionado " + str(count) + " veces.")
         arduino.write('A')
-        estado = arduino.readline()
         print(estado)
         time.sleep(.3) #DEBOUNCE
+        estado = arduino.readline()
+        time.sleep(.32)
+    signal.signal(signal.SIGINT, signal_handler)
     time.sleep(.01)
-
-GPIO.cleanup()
